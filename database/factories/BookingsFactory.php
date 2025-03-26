d3c19e810a159c1c97beef78e4a2660e61bf3bde
Pablosudev <pablo.losu.dev@gmail.com>
2025-03-26 16:29:21 +0000

    post AllFactory

--- database/factories/BookingsFactory.php
+++ database/factories/BookingsFactory.php
@@ -3,13 +3,25 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\BookingsModel;
+use App\Models\RoomsModel;
 
 
 class BookingsFactory extends Factory
 {
    
+    protected $model = BookingsModel::class;
     public function definition(): array
     {
-     protected $model = BookingsModel::class;
+        return [
+            'id' => $this->faker->unique()->randomNumber(), 
+            'name' => $this->faker->name(), 
+            'date' => $this->faker->date(), 
+            'check_in' => $this->faker->dateTimeThisYear(), 
+            'check_out' => $this->faker->dateTimeBetween('+1 day', '+7 days'), 
+            'request' => $this->faker->sentence(), 
+            'status' => $this->faker->randomElement(['Check In', 'Check Out', 'In progress']),
+            'room_id' => Room::inRandomOrder()->first()->id, 
+        ]
     }
 }
--- database/factories/RoomtFactory.php
+++ database/factories/RoomtFactory.php
@@ -3,11 +3,22 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\RoomsModel;
 
 class RoomtFactory extends Factory
 {
+
+    protected $model = RoomsModel::class;
     public function definition(): array
     {
-       protected $model = RoomsModel::class;
+        return [
+            'id' => $this->faker->unique()->randomNumber(), 
+            'number' => $this->faker->unique()->randomNumber(3), 
+            'price' => $this->faker->randomFloat(100, 200, 500), 
+            'offer' => $this->faker->boolean(), 
+            'roomStatus' => $this->faker->randomElement(['Available', 'Booked']), 
+            'type' => $this->faker->randomElement(['Single-bed', 'Double-bed', 'Suite', 'Double Superior']), 
+            'amenities' => $this->faker->words(7, true), 
+        ];
     }
 }
