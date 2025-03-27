4fbd97033edab39a184d21a0c07ba0228a4d5d1a
Pablosudev <pablo.losu.dev@gmail.com>
2025-03-27 23:00:33 +0000

    post ContactController

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ContactController;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        $contacts = Contact::all();
+        return view('contacts',['contacts' => $contacts]);
+    }
+    public function create();
+    {
+        $contacts = Contact::all()
+        return view('contacts', ['contacts' => $contacts])
+    }
+    public function store(Request $request)
+    {
+        $validate =$request->validate([
+            'date' => 'required|date',
+            'name' => 'required|string',
+            'email' => 'required\string',
+            'phone' => 'required|string',
+            'subject' => 'required|string',
+            'comment' => 'required|string',
+            'archived' => 'required|boolean',
+        ])
+
+        $contacts = new Contact();
+        $contacts ->date = $validated['date'];
+        $contacts ->name = $validated['name'];
+        $contacts ->email = $validated['email'];
+        $contacts ->phone = $validated['phone'];
+        $contacts ->subject = $validated['subject'];
+        $contacts ->comment = $validated['comment'];
+        $contacts ->archived = $validated['archived'];
+
+        $contacts->save();
+        return ('Contact create.')
+    }
+    public function show(string $id)
+    {
+        $contacts = Contact::findOrFail($id);
+        retunr response()->json($contacts)
+    }
+    public function edit(string $id)
+    {
+        
+    }
+    public function update(Request $request,string $id)
+    {
+
+    }
+    public function destroy(string $id)
+    {
+        try {
+            $contacts = Contact::find($id);
+            $contacts->delete();
+            return response()->json['message' => 'Contact delete'];
+        } catch (\Throwable $th) {
+            return('Delete not found');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,33 @@
 <?php
-use App\Http\Controllers\Resource;
-use App\Http\Controllers\UserController;
+
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Controllers\Resource;
+use App\Controllers\UserController;
+use App\Controllers\ContactController;
+use App\Controllers\RoomController;
+use App\Controllers\BookingController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('activities', Resource::class);
-Route::resource('users', UserController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('activities',Resource::class);
+    Route::resource('users',UserController::class);
+    Route::resource('contacts',ContactController::class);
+    Route::resource('rooms', RoomController::class);
+    Route::resource('bookings',BookingController::class);
+    
+});
+
+
+require __DIR__.'/auth.php';
