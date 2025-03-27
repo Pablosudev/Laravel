0764631cb429d7ba7f126c11e934632fc8a7dddd
Pablosudev <pablo.losu.dev@gmail.com>
2025-03-27 23:15:23 +0000

    post migrateRooms

---
+++ database/migrations/2025_03_27_230126_create_rooms.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->integer('number');
+            $table->integer('price');
+            $table->integer('offer');
+            $table->enum('roomStatus', ['Available', 'Booked']) -> default ('Available');
+            $table->enum('type', ['Suite', 'Double Bed', 'Double Superior', 'Single Bed']);
+            $table->enum('amenities', ['WIFI', 'AC', 'Shower', 'Pool', 'TV', 'Parking', 'GYM', 'Bar', 'Breakfast']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
